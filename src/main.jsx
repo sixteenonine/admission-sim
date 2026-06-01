32394fb3e179d7e0df52d9a38ea5971ac75ca287
sixteenonine <sixteenonine99@gmail.com>
2026-06-01 08:59:35 +0700

    KVlast

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // แคชข้อมูลไว้ 5 นาที ลดภาระเซิร์ฟเวอร์ D1
-      refetchOnWindowFocus: false, // ปิดการดึงข้อมูลซ้ำเวลาเด็กสลับแท็บเบราว์เซอร์ไปมา
+      staleTime: 5 * 60 * 1000, 
+      refetchOnWindowFocus: false, 
     },
   },
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId="624668257674-mmdola6hm6rsep9ddcmqi51h84qa5dko.apps.googleusercontent.com">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
